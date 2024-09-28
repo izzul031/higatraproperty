546446c7e4520f0ed4c0960d3457bd18a3766dc2
Nur Mu'izzul Qodir <izzulqodir031@gmail.com>
2024-09-28 11:01:30 +0700

    update

--- home-login.php
+++ home-login.php
@@ -26,41 +26,49 @@
 </head>
 
 <body>
-<section class="vh-100">
-  <div class="container py-5 h-100">
-    <div class="row d-flex align-items-center justify-content-center h-100">
-      <div class="col-md-8 col-lg-7 col-xl-6">
-        <img src="loginbaru.png" class="img-fluid" alt="Phone image" height="300px" width="600px" style="margin-left: -41px">
+  <section class="vh-100">
+      <div class="container py-5 h-100">
+        <div class="row d-flex align-items-center justify-content-center h-100">
+          <div class="col-md-8 col-lg-7 col-xl-6">
+            <img src="loginbaru.png" class="img-fluid" alt="Phone image" height="300px" width="600px" style="margin-left: -41px">
+          </div>
+          <div class="col-md-7 col-lg-5 col-xl-5">
+    <form class="form_login" action="login.php" method="post">
+      <p class="text-center h8 fw-bold mb-4 mx-1 mx-md-3 mt-3">WELCOME TO</p>
+
+      <img src="../higatra/images/Group397.png" alt="Deskripsi gambar" width="250" height="auto" class="img-fluid mb-4" style="display: block; margin-left: auto; margin-right: auto; margin-bottom:50px;">
+
+      <div class="form-outline mb-4 input-group">
+      <input type="email" id="form1Example13" class="form-control form-control-lg py-3" name="username"autocomplete="off" placeholder="E-mail" />
       </div>
-      <div class="col-md-7 col-lg-5 col-xl-5">
-<form class="form_login" action="login.php" method="post">
-  <p class="text-center h8 fw-bold mb-4 mx-1 mx-md-3 mt-3">WELCOME TO</p>
 
-  <img src="../higatra/images/Group397.png" alt="Deskripsi gambar" width="250" height="auto" class="img-fluid mb-4" style="display: block; margin-left: auto; margin-right: auto; margin-bottom:50px;">
+      <div class="form-outline mb-4 input-group">
+        <input type="password" id="form1Example23" class="form-control form-control-lg py-3" name="password" autocomplete="off" placeholder="Password" />
+      </div>
 
-  <div class="form-outline mb-4 input-group">
-    <input type="email" id="form1Example13" class="form-control form-control-lg py-3" name="username" autocomplete="off" placeholder="E-mail" />
-  </div>
-
-  <div class="form-outline mb-4 input-group">
-    <input type="password" id="form1Example23" class="form-control form-control-lg py-3" name="password" autocomplete="off" placeholder="Password" />
-  </div>
-
-  <div class="d-flex justify-content-center input-group">
-    <input type="submit" value="LOGIN" name="login" class="btn btn-custom2 btn-block btn-lg text-light my-2 py-3" />
-  </div>
-</form>
+      <div class="d-flex justify-content-center input-group">
+        <input type="submit" value="LOGIN" name="login" class="btn btn-custom2 btn-block btn-lg text-light my-2 py-3" />
+      </div>
+    </form>
 
       </div>
     </div>
   </div>
 </section>
-
-
-
   </div>
 
+<script>
+  function validateForm() {
+    var username = document.getElementById("form1Example13").value;
+    var password = document.getElementById("form1Example23").value;
 
+    if (username === "" || password === "") {
+      alert("Mohon isi semua kolom sebelum login.");
+      return false; // Menghentikan form dari pengiriman
+    }
+    return true; // Melanjutkan pengiriman form
+  }
+</script>
  
 
 
